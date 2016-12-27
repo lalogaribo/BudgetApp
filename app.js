8682dfe073d37bc2ca7459e3a3b27effff14375d
lalogaribo <lalo.garibo90@gmail.com>
2016-12-27 20:29:33 +0000

    creation of controllers

---
+++ app.js
@@ -0,0 +1,24 @@
+//Budget Controller
+var budgetController = (function () {
+
+})();
+
+//UI Controller
+var UIController = (function () {
+
+
+
+
+})();
+
+
+
+
+//Global app Controller
+var controller = (function (budgetCtrl, UICtrl) {
+
+
+
+
+
+})(budgetController,UIController);
